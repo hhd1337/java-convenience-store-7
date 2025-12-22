418f1cf001c371033c587c91dbdc1acb6096468a
goeka <goeka1337@gmail.com>
2025-12-22 16:01:43 +0900

    refactor(domain): 결제 계산기가 주문/재고를 주입받도록 의존성 추가
    - 주문 결제 계산을 위해 PaymentCalculator에 Order, Stock 의존성을 주입
    - calculateTotalPurchaseAmount의 파라미터를 제거하고 생성자 주입으로 변경

--- src/main/java/store/domain/PaymentCalculator.java
+++ src/main/java/store/domain/PaymentCalculator.java
@@ -1,17 +1,25 @@
 package store.domain;
 
 public class PaymentCalculator {
-    public int calculateTotalPurchaseAmount(Order order, Stock stock) {
+    private final Order order;
+    private final Stock stock;
+
+    public PaymentCalculator(Order order, Stock stock) {
+        this.order = order;
+        this.stock = stock;
+    }
+
+    public int calculateTotalPurchaseAmount() {
         // 재고 검증, 예외로직
         order.getOrderItems()
                 .forEach(item -> stock.hasEnoughStock(item.getName(), item.getQuantity()));
 
         return order.getOrderItems().stream()
-                .mapToInt(item -> calculateItemPurchaseAmount(item, stock))
+                .mapToInt(this::calculateItemPurchaseAmount)
                 .sum();
     }
 
-    private int calculateItemPurchaseAmount(OrderItem item, Stock stock) {
+    private int calculateItemPurchaseAmount(OrderItem item) {
         int unitPrice = stock.findPriceByName(item.getName());
         return item.calculateItemTotalPrice(unitPrice);
     }
