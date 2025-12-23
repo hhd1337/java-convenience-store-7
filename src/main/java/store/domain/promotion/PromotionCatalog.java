cd261d77930c0f74289919966f07c400c149a806
goeka <goeka1337@gmail.com>
2025-12-24 04:08:25 +0900

    feat(domain): Promotion 컬렉션을 감싸는 PromotionCatalog 도메인 클래스 추가
    - Promotion 리스트를 도메인 객체로 감싸 외부에서 컬렉션을 직접 다루지 않도록 캡슐화
    - 프로모션관련 규칙(일급컬렉션)과 조회로직(Catalog)을 이 클래스에 모으도록 구조 마련

---
+++ src/main/java/store/domain/promotion/PromotionCatalog.java
@@ -0,0 +1,12 @@
+package store.domain.promotion;
+
+import java.util.List;
+
+public class PromotionCatalog {
+    private List<Promotion> promotions;
+
+    public PromotionCatalog(List<Promotion> promotions) {
+        this.promotions = promotions;
+    }
+
+}
