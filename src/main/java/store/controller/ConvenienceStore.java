5ae9513175e99d4c01cdab264a981ed341b9eb88
goeka <goeka1337@gmail.com>
2025-12-24 04:25:17 +0900

    feat(controller): 프로모션, 상품 데이터 로드

--- src/main/java/store/controller/ConvenienceStore.java
+++ src/main/java/store/controller/ConvenienceStore.java
@@ -1,5 +1,12 @@
 package store.controller;
 
+import java.util.List;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.PromotionCatalog;
+import store.domain.stock.Product;
+import store.domain.stock.Stock;
+import store.file.ProductFileReader;
+import store.file.PromotionFileReader;
 import store.view.InputView;
 import store.view.OutputView;
 
@@ -13,5 +20,11 @@ public class ConvenienceStore {
     }
 
     public void open() {
+        List<Product> products = ProductFileReader.read();
+        Stock stock = new Stock(products);
+
+        List<Promotion> promotions = PromotionFileReader.read();
+        PromotionCatalog promotionCatalog = new PromotionCatalog(promotions);
+
     }
 }
