8918d0d68d320a57899b5b5a37ec7f0ec6612a4b
goeka <goeka1337@gmail.com>
2025-12-24 13:57:23 +0900

    feat(view): 보유 상품목록 및 안내문구 출력 구현

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,4 +1,14 @@
 package store.view;
 
+import java.util.List;
+
 public class OutputView {
+    public void printStock(List<String> lines) {
+        System.out.println("안녕하세요. w편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+        for (String line : lines) {
+            System.out.println("- " + line);
+        }
+        System.out.println("\n구매하실 상품명과 수량을 입력해주세요. (예: [사이다-2],[감자칩-1])");
+    }
 }
