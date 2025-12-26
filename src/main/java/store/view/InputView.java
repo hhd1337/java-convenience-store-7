bcb9a6d121eef6ca3f82b94a1b8be0231fdd880f
goeka <goeka1337@gmail.com>
2025-12-27 05:23:24 +0900

    feat(view): 상품명, 수량 입력받아 Map에 담아 반환하는 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,4 +1,33 @@
 package store.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import store.util.ErrorMessage;
+
 public class InputView {
+
+    public Map<String, Integer> readProductNameAndQuantity() {
+        String input = Console.readLine();
+        Map<String, Integer> quantitiesByName = new LinkedHashMap<>();
+
+        String[] tokens = input.split(",");
+        Arrays.stream(tokens)
+                .map(String::trim)
+                .map(s -> s.replace("[", "").replace("]", ""))
+                .forEach(item -> {
+                    String[] parts = item.split("-");
+                    String name = parts[0].trim();
+                    int quantity = Integer.parseInt(parts[1].trim());
+
+                    if (quantitiesByName.containsKey(name)) {
+                        throw new IllegalArgumentException(ErrorMessage.PREFIX + "같은 상품은 합쳐서 한번만 입력해주세요: " + name);
+                    }
+
+                    quantitiesByName.put(name, quantity);
+                });
+
+        return quantitiesByName;
+    }
 }
